9ddeb94c595a208f02b7343eebf6907315444d74
Inaldo <113635074+inaldopf@users.noreply.github.com>
2026-02-24 11:13:20 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -11,3 +11,14 @@ export const categoriesList = [
     "Salário", "Investimento", "Aluguel", "Comida", "Transporte", "Lazer", 
     "Saúde", "Educação", "Casa", "Compras", "Outros"
 ];
+
+// --- NOVO: FUNÇÃO DE SEGURANÇA ANTI-XSS ---
+export const escapeHTML = (str) => {
+    if (!str) return '';
+    return str.toString()
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+};
